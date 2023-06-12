092eb12d6290f86a3c86a7a5f7c16aa123c3f652
hichamid4 <hicham.idhmed20@gmail.com>
2023-06-12 11:21:43 +0100

    t

--- resources/views/components/liveModal.blade.php
+++ resources/views/components/liveModal.blade.php
@@ -1,26 +1,28 @@
-@props(['formAction' => false])
+<div class="flex items-center justify-center min-h-screen  px-4 pt-4 pb-10 text-center">
+  @props(['formAction' => false])
 
-<div>
-  @if($formAction)
-  <form wire:submit.prevent="{{ $formAction }}">
-    @endif
-    <div class="bg-white p-4 sm:px-6 sm:py-4 border-b border-gray-150">
-      @if(isset($title))
-      <h3 class="text-lg leading-6 font-medium text-gray-900">
-        {{ $title }}
-      </h3>
-      @endif
-    </div>
-    <div class="bg-white px-4 sm:p-6">
-      <div class="space-y-6">
-        {{ $content }}
-      </div>
-    </div>
-
-    <div class="bg-white px-4 pb-5 sm:px-4 sm:flex">
-      {{ $buttons }}
-    </div>
+  <div>
     @if($formAction)
-  </form>
-  @endif
+    <form wire:submit.prevent="{{ $formAction }}">
+      @endif
+      <div class="bg-white p-4 sm:px-6 sm:py-4 border-b border-gray-150">
+        @if(isset($title))
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          {{ $title }}
+        </h3>
+        @endif
+      </div>
+      <div class="bg-white px-4 sm:p-6">
+        <div class="space-y-6">
+          {{ $content }}
+        </div>
+      </div>
+
+      <div class="bg-white px-4 pb-5 sm:px-4 sm:flex">
+        {{ $buttons }}
+      </div>
+      @if($formAction)
+    </form>
+    @endif
+  </div>
 </div>
